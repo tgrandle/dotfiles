668e7580429f0c57330519b920411266f37f8fe5
Trevor Grandle <tgrandle@gmail.com>
2017-12-20 09:47:52 -0500

    git config

--- run-me.ubuntu.sh
+++ run-me.ubuntu.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-sudo apt-get update
+sudo apt-get -qq update
 
-sudo apt-get install \
-    open-ssh-server \
+sudo apt-get -y install \
+    openssh-server \
     tmux \
     zsh \
 
 # ohmyzsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-git clone https://github.com/tgrandle/dotfiles
-mv dotfiles/tmux.conf ~/.tmux.conf
+cp ~/dotfiles/tmux.conf ~/.tmux.conf
 
+git config --global user.email "tgrandle@gmail.com"
+git config --global user.name "Trevor Grandle"
+
+#### TO-DO
+# install ssh keys
